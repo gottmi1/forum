8d9994d2fb4723c151d7e2caa692f54ede2b1bac
gottmi1 <91591041+gottmi1@users.noreply.github.com>
2022-04-10 04:55:01 +0900

    수정시 현재 업로드된 이미지 크기를 썸네일 크기로 변환

--- models/forum.js
+++ models/forum.js
@@ -2,16 +2,23 @@ const mongoose = require("mongoose");
 const Comment = require("./comment");
 const Schema = mongoose.Schema;
 
+const imgSchema = new Schema({
+  url: String,
+  filename: String,
+});
+imgSchema.virtual("thumbnail").get(function () {
+  return this.url.replace("/upload", "/upload/w_150");
+  // /upload를 /upload/w_300으로 대체해 width 150의 이미지로 바꾼다
+});
+// 가상특성을 추가하기 위해 imgs의 내부를 따로 스키마로 만듬(가상 특성은 스키마 단위에만 추가할 수 있음)
+// virtual을 사용한 이유는 저장된 정보를 토대로 만들어지기 때문에 새로 모델이나 데이터베이스에 따로 저장할 필요가 없어서
+// thumbnail을 호출할 때 마다 이 코드가 실행된다
+
 const ForumSchema = new Schema({
   title: String,
   contents: String,
   date: String,
-  imgs: [
-    {
-      url: String,
-      filename: String,
-    },
-  ],
+  imgs: [imgSchema],
   author: {
     type: Schema.Types.ObjectId,
     ref: "User",
